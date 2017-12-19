9ac0eb401ca408050b7b5454144f1e59fe9850e0
Marc Hammerton <m.hammerton@aims.gov.au>
2017-12-19 11:05:38 +1000

    initial commit; working openlayers edit field and view

---
+++ js/eatlas_map_field_edit.js
@@ -0,0 +1,59 @@
+/**
+ * Provide an editable map for creating an overview map
+ */
+(function ($) {
+	$(document).ready(function () {
+		var geoJsonWriter = new ol.format.GeoJSON();
+
+		var raster = new ol.layer.Tile({
+			source: new ol.source.OSM()
+		});
+
+		var source = new ol.source.Vector({
+			format: new ol.format.GeoJSON()
+		});
+
+		var vector = new ol.layer.Vector({
+			source: source
+		});
+
+		var map = new ol.Map({
+			layers: [raster, vector],
+			target: 'map',
+			view: new ol.View({
+				center: [15000000, -3350000],
+				zoom: 4
+			})
+		});
+
+		var geoJsonTextField = $(map.getTargetElement()).closest('.field-type-eatlas-map-field').find('.edit-map-field-editable-map');
+		if (geoJsonTextField.val()) {
+			source.addFeatures(geoJsonWriter.readFeatures(geoJsonTextField.val()));
+		}
+
+		var draw = new ol.interaction.Draw({
+			source: source,
+			type: 'Polygon'
+		});
+		map.addInteraction(draw);
+
+		var modify = new ol.interaction.Modify({
+			source: source
+		});
+		map.addInteraction(modify);
+
+		var snap = new ol.interaction.Snap({
+			source: source
+		});
+		map.addInteraction(snap);
+
+		vector.on('change', function (map, source) {
+			return function (event) {
+				console.log(source);
+				geoJsonTextField.val(geoJsonWriter.writeFeatures(source.getFeatures()));
+				return true;
+			};
+		}(map, source));
+
+	});
+}(jQuery));
---
+++ js/eatlas_map_field_view.js
@@ -0,0 +1,36 @@
+/**
+ * Provide an editable map for creating an overview map
+ */
+(function ($) {
+	$(document).ready(function () {
+		var geoJsonWriter = new ol.format.GeoJSON();
+
+		$('.eatlas_map_field_map').each(function () {
+				var geoJsonObject = $(this).data('map-overview-geoJson');
+
+				var raster = new ol.layer.Tile({
+					source: new ol.source.OSM()
+				});
+
+				var source = new ol.source.Vector({
+					format: new ol.format.GeoJSON(),
+					features: geoJsonWriter.readFeatures(geoJsonObject)
+				});
+
+				var vector = new ol.layer.Vector({
+					source: source
+				});
+
+				new ol.Map({
+					layers: [raster, vector],
+					target: $(this)[0],
+					view: new ol.View({
+						center: [15000000, -3350000],
+						zoom: 4
+					})
+				});
+
+			}
+		);
+	});
+}(jQuery));
