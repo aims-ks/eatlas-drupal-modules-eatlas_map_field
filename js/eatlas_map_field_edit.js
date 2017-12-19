a396a61f65c209fe8d894bce947cfe1916730245
Marc Hammerton <m.hammerton@aims.gov.au>
2017-12-19 14:25:03 +1000

    rearrange JS code

--- js/eatlas_map_field_edit.js
+++ js/eatlas_map_field_edit.js
@@ -2,51 +2,61 @@
  * Provide an editable map for creating an overview map
  */
 (function ($) {
-	$(document).ready(function () {
-		var geoJsonWriter = new ol.format.GeoJSON();
+	/**
+	 * Define a namespace for the application.
+	 */
+	window.eatlasMapFieldApp = {};
+	var eatlasMapFieldApp = window.eatlasMapFieldApp;
+	eatlasMapFieldApp.map = {};
+	eatlasMapFieldApp.source = {};
+	eatlasMapFieldApp.vector = {};
 
+	/**
+	 * Initialise the source, vector and map
+	 */
+	eatlasMapFieldApp.init = function() {
 		var raster = new ol.layer.Tile({
 			source: new ol.source.OSM()
 		});
 
-		var source = new ol.source.Vector({
+		eatlasMapFieldApp.source = new ol.source.Vector({
 			format: new ol.format.GeoJSON()
 		});
 
-		var vector = new ol.layer.Vector({
-			source: source,
+		eatlasMapFieldApp.vector = new ol.layer.Vector({
+			source: eatlasMapFieldApp.source,
 			wrapX: false
 		});
 
-		var map = new ol.Map({
-			layers: [raster, vector],
+		eatlasMapFieldApp.map = new ol.Map({
+			layers: [raster, eatlasMapFieldApp.vector],
 			target: 'eatlas-map-field-map',
 			view: new ol.View({
 				center: [15000000, -3350000],
 				zoom: 4
 			})
 		});
+	};
 
-		var geoJsonTextField = $(map.getTargetElement()).closest('.field-type-eatlas-map-field').find('.edit-map-field-textarea');
-		if (geoJsonTextField.val()) {
-			source.addFeatures(geoJsonWriter.readFeatures(geoJsonTextField.val()));
-		}
-
+	/**
+	 * Enable all interactions interaction
+	 */
+	eatlasMapFieldApp.enableInteractions = function() {
 		var draw = new ol.interaction.Draw({
-			source: source,
+			source: eatlasMapFieldApp.source,
 			type: 'Polygon'
 		});
-		map.addInteraction(draw);
+		eatlasMapFieldApp.map.addInteraction(draw);
 
 		var modify = new ol.interaction.Modify({
-			source: source
+			source: eatlasMapFieldApp.source
 		});
-		map.addInteraction(modify);
+		eatlasMapFieldApp.map.addInteraction(modify);
 
 		var snap = new ol.interaction.Snap({
-			source: source
+			source: eatlasMapFieldApp.source
 		});
-		map.addInteraction(snap);
+		eatlasMapFieldApp.map.addInteraction(snap);
 
 		var select = new ol.interaction.Select({
 			condition: function(mapBrowserEvent) {
@@ -54,15 +64,33 @@
 					ol.events.condition.altKeyOnly(mapBrowserEvent);
 			}
 		});
-		map.addInteraction(select);
+		eatlasMapFieldApp.map.addInteraction(select);
+	};
 
-		vector.on('change', function (map, source) {
+
+	/**
+	 * When DOM is ready, start map field application
+	 */
+	$(document).ready(function () {
+
+		eatlasMapFieldApp.init();
+		eatlasMapFieldApp.enableInteractions();
+
+		var geoJsonWriter = new ol.format.GeoJSON();
+
+		// load existing features
+		var geoJsonTextField = $(eatlasMapFieldApp.map.getTargetElement()).closest('.field-type-eatlas-map-field').find('.edit-map-field-textarea');
+		if (geoJsonTextField.val()) {
+			eatlasMapFieldApp.source.addFeatures(geoJsonWriter.readFeatures(geoJsonTextField.val()));
+		}
+
+		// write new GeoJson to text area
+		eatlasMapFieldApp.vector.on('change', function (source) {
 			return function (event) {
-				console.log(source);
-				geoJsonTextField.val(geoJsonWriter.writeFeatures(source.getFeatures()));
+				geoJsonTextField.val(geoJsonWriter.writeFeatures(eatlasMapFieldApp.source.getFeatures()));
 				return true;
 			};
-		}(map, source));
+		}(eatlasMapFieldApp.source));
 
 	});
 }(jQuery));
